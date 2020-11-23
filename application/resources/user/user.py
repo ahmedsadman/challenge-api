06a26892a8560bfdfcf2904f0457943e9b249ff2
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-11-23 13:31:34 +0600

    get user challenges route

--- application/resources/user/user.py
+++ application/resources/user/user.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from . import bp
 from application.models import User, Challenge
-from application.error_handlers import UserAlreadyExists, NotFound
+from application.error_handlers import UserAlreadyExists, NotFound, BadRequest
 
 
 @bp.route("/<user_id>", methods=["GET"])
@@ -32,6 +32,40 @@ def get_follower_list(user_id):
     raise NotFound("User does not exist")
 
 
+@bp.route("/follow/<follow_id>", methods=["POST"])
+@jwt_required
+def follow(follow_id):
+    me = User.query.get(get_jwt_identity())
+    user = User.query.get(follow_id)
+
+    if user is None:
+        raise NotFound("User not found")
+
+    if not me.is_following(user):
+        me.follow(user)
+
+    me.save()
+    return {"message": "DONE!"}
+
+
+@bp.route("/unfollow/<follow_id>", methods=["POST"])
+@jwt_required
+def unfollow(follow_id):
+    me = User.query.get(get_jwt_identity())
+    user = User.query.get(follow_id)
+
+    if user is None:
+        raise NotFound("User not found")
+
+    if me.is_following(user):
+        me.unfollow(user)
+    else:
+        raise BadRequest("The user is not being followed by you")
+
+    me.save()
+    return {"message": "DONE!"}
+
+
 @bp.route("/feed", methods=["GET"])
 @jwt_required
 def get_user_feed():
@@ -43,3 +77,10 @@ def get_user_feed():
     return Challenge.serialize_feed(user, feed)
 
     raise NotFound("User not found")
+
+
+@bp.route("/my-challenges", methods=["GET"])
+@jwt_required
+def get_my_challenges():
+    user = User.query.get(get_jwt_identity())
+    return {"challenges": [item.serialize() for item in user.created_challenges.all()]}
